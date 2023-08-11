62b0f2e42b41f68c7ab7169440a248a2dc2bbfdd
Vishal Karthikeyan <133529170+CrompBreeze@users.noreply.github.com>
2023-08-11 19:56:32 +0530

    Added playRound() function

--- myscript.js
+++ myscript.js
@@ -3,4 +3,30 @@ function computerChoice() {
     return choices[Math.floor(Math.random()*3)];
 
 }
-console.log(computerChoice());
+function playRound(playerChoice,computerSelection) {
+    computerSelection = computerChoice();
+    playerChoice = prompt('What is your choice ?');
+    console.log('Computer chose ' + computerSelection);
+    if (playerChoice == 'Rock' && computerSelection == 'Paper') {
+        return 'You Lose! Paper beats Rock'
+    }
+    else if (playerChoice == 'Rock' && computerSelection == 'Scissors') {
+        return 'You Win! Rock beats Scissors'
+    }
+    else if (playerChoice == 'Paper' && computerSelection == 'Rock') {
+        return 'You Win! Paper beats Rock'
+    }
+    else if (playerChoice == 'Paper' && computerSelection == 'Scissors') {
+        return 'You Lose! Scissors beats Paper'
+    }
+    else if (playerChoice == 'Scissors' && computerSelection == 'Rock') {
+        return 'You Lose! Rock beats Scissors'
+    }
+    else if (playerChoice == 'Scissors' && computerSelection == 'Paper') {
+        return 'You Win! Scissors beats Paper'
+    }
+    else {
+        return 'Draw!'
+    }
+}
+console.log(playRound());
